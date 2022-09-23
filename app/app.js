effde9ca037673ee09b96fea23b9d7f1e73ce8f3
jinseongbe <jinseongbe@naver.com>
2022-09-23 14:09:21 +0900

    js front done

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
+app.use(express.static(`${__dirname}/src/public`)); // __dirname 현재 파일의 경로를 반환함
 
 module.exports = app;
 
